6609be78ce92c7267945674ac92334df8662d1f6
villa0505 <villa.nhea@student.passerellesnumeriques.org>
2023-04-18 16:22:49 +0700

    undate Maint

--- src/Main.ts
+++ src/Main.ts
@@ -5,8 +5,10 @@ import { MiniVan } from "./MiniVan";
 let batmobile1 = new BatMobile("500", 110, true )
 console.log(batmobile1);
 
-let batmobile1 = new BatMobile("500", 21, true )
-let minivan = new MiniVan("2002",45,2,1)
+let batmobile2 = new BatMobile("500", 21, true )
+
+let minivan = new MiniVan("2002", 45, 2, 1)
 console.log(batmobile1);
+console.log(batmobile2);
 console.log(minivan);
 
